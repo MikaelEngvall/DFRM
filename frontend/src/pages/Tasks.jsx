38edb1378173e13cffd6a23dead11208e8149233
Mikael Engvall <mikael.engvall.me@gmail.com>
2025-04-15 04:43:26 +0200

    Add delete button in edit mode for selected tasks in Tasks component

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -970,6 +970,22 @@ const Tasks = () => {
               </select>
             </div>
           )}
+
+          {/* Lägg till radera-knapp om vi är i redigeringsläge */}
+          {selectedTask && (
+            <div className="mt-5 pt-4 border-t border-gray-200 dark:border-gray-700">
+              <button
+                type="button"
+                onClick={() => handleDelete(selectedTask)}
+                className="px-4 py-2 bg-red-600 text-white rounded-md hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-800 flex items-center"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                </svg>
+                {t('common.delete')}
+              </button>
+            </div>
+          )}
         </div>
       </Modal>
       
